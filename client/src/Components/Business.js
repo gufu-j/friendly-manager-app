fc94ec8cdafcd919746ddaeb6be31cc6a3786c76
diego <guatemaladiegojose@gmail.com>
2023-12-07 22:42:13 -0500

    left things back like they were about routing

--- client/src/Components/Business.js
+++ client/src/Components/Business.js
@@ -6,7 +6,7 @@ function Business({store}){
 
     return(
            <div className="cardLowManager">
-                <NavLink style={{paddingLeft: 13, textDecoration: 'none'}} to={"/home_freeport/orders"}>
+                <NavLink style={{paddingLeft: 13, textDecoration: 'none'}} to={"/orders"}>
                     <div className="container">
                         <h1 className="store-name"> Home Freepot {store.id}</h1>
                         <p> Location : {store.location}</p>
