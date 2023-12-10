87f6736320ad2e11bde363e3431ed9b4f3d6501f
diego <guatemaladiegojose@gmail.com>
2023-12-10 18:31:01 -0500

    added new code to production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,4 +125,22 @@ Rails.application.configure do
 #   :enable_starttls_auto => true
 # }
 
+
+  # SMTP settings for gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    user_name: "guatemaladiegojose@gmail.com",
+    password: "dpiy suzm jmhw jngf",
+    authentication: "plain",
+    enable_starttls: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
+
+
+
+
 end
