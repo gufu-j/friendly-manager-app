3c2b33e506f810a1ee9d531004842c18ca9e800f
diego <guatemaladiegojose@gmail.com>
2023-12-11 18:49:31 -0500

    modified validation for Orders' models

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,12 +8,8 @@ class Order < ApplicationRecord
 
     def orders_per_day_unic
         existing = Order.where(product_id: self.product_id).where(store_id: self.store_id).where(created_at: Date.current.all_day)
-        errors.add(:product_id, 'you have already created this product today, go to your orders, check it out and edit it if you wish to order more') if !existing.empty?
+        errors.add(:product_id, 'has already been created today, check it out on your orders and edit it if you wish to order more') if !existing.empty?
     end
 
-    # def order_repit
-    #     existing = Order.where(product_id: self.product_id).where(created_at: Time.current.all_day)
-    #     errors.add(:name, 'you already order this today, check orders and edit it') if existing
-    # end
 
 end
