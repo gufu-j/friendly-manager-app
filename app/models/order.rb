99e36b2b0013dbac589f4fab2acf8bfb06e121cc
diego <guatemaladiegojose@gmail.com>
2023-12-11 15:06:08 -0500

    added customized validation for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,19 +4,14 @@ class Order < ApplicationRecord
 
     validates :quantity, presence: true
     # validates :note, presence: true
-    # validates :orders_per_day_unic
+    validate :orders_per_day_unic
 
-    # def orders_per_day_unic
-    #     existing = Order.where(product_id: self.product_id).where(created_at: Date.current)
-    #     errors.add(:product_id, 'cannot be today') if existing
-    # end
-
-
-
-
+    def orders_per_day_unic
+        existing = Order.where(product_id: self.product_id).where(store_id: self.store_id).where(created_at: Date.current.all_day)
+        errors.add(:product_id, 'cannot be today') if !existing.empty?
+    end
 
     # def order_repit
-        
     #     existing = Order.where(product_id: self.product_id).where(created_at: Time.current.all_day)
     #     errors.add(:name, 'you already order this today, check orders and edit it') if existing
     # end
